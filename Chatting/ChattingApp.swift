f65f3e172215f3e125c55a4551f911b7818dcff5
Ofdikili <omerfarukdikili00@gmail.com>
2025-02-22 23:56:22 +0300

    Initial Commit

---
+++ Chatting/ChattingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChattingApp.swift
+//  Chatting
+//
+//  Created by Ömer Faruk Dikili on 22.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct ChattingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChattingTests/ChattingTests.swift
@@ -0,0 +1,17 @@
+//
+//  ChattingTests.swift
+//  ChattingTests
+//
+//  Created by Ömer Faruk Dikili on 22.02.2025.
+//
+
+import Testing
+@testable import Chatting
+
+struct ChattingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
