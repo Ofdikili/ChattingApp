514d1c73935332d7af11dcc492e700484fb2f487
Ofdikili <omerfarukdikili00@gmail.com>
2025-02-25 01:58:59 +0300

    feat: Add CommunitiesView with UI layout for community interactions
    - Implement `CommunitiesView` using `NavigationStack` and `ScrollView`
    - Add image, text descriptions, and buttons for community interactions
    - Create a reusable `createNewCommunityButton` for adding new communities
    - Apply styling with padding, background color, and rounded corners

--- Chatting/Core/Communities/View/CommunitiesView.swift
+++ Chatting/Core/Communities/View/CommunitiesView.swift
@@ -9,7 +9,37 @@ import SwiftUI
 
 struct CommunitiesView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            ScrollView{
+                VStack(alignment: .leading, spacing: 8){
+                    Image("communities")
+                    Group{
+                        Text("Stay connected with a community")
+                        Text("Communities bring members together in topic based groups. Any community you're added to will appear here.")
+                            .foregroundStyle(.gray)
+                    }
+                    .padding(.horizontal,5)
+                    Button("See Example communities>"){
+                        
+                    }.frame(maxWidth:.infinity,alignment: .center)
+                    createNewComminityButton()
+                 
+                }
+            }
+            .navigationTitle("Communities")
+        }
+    }
+    
+    fileprivate func createNewComminityButton() -> Button<some View> {
+        return Button(action: {},label: {
+            Label("New Community",systemImage: "plus")
+                .frame(maxWidth:.infinity)
+                .padding(16)
+                .background(Color.blue)
+                .foregroundStyle(.white)
+                .clipShape(RoundedRectangle(cornerRadius: 8))
+                .padding()
+        })
     }
 }
 
