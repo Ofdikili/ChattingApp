babe646431aee022d270f03ce15e4e6d844ea0f9
Ofdikili <omerfarukdikili00@gmail.com>
2025-02-25 17:37:34 +0300

    chore: update .gitignore to exclude GoogleService-Info.plist
    - Added `GoogleService-Info.plist` to .gitignore to prevent committing Firebase config.

--- Chatting/ChattingApp.swift
+++ Chatting/ChattingApp.swift
@@ -7,11 +7,31 @@
 
 import SwiftUI
 
+import SwiftUI
+import FirebaseCore
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
+
 @main
 struct ChattingApp: App {
-    var body: some Scene {
-        WindowGroup {
-            MainTabView()
-        }
+  // register app delegate for Firebase setup
+  @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
+
+  var body: some Scene {
+    WindowGroup {
+      NavigationView {
+          MainTabView()
+      }
     }
+  }
 }
+
